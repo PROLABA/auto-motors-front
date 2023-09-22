c963b9d3e8cdb496bd1b3a990f3cf360df7936c8
Kistanov Daniil <rizzydizzy@mail.ru>
2023-09-22 15:34:24 +0300

    Каталог + структура детальной

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,41 @@ let cardSliders = new Swiper('.card__gallery', {
       el: '.swiper-pagination'
   }
 })
+let pageHeadBanners = new Swiper('.page-head__banners', {
+  loop:true,
+  spaceBetween:0,
+  pagination: {
+      el: '.swiper-pagination'
+  }
+})
+let productGallery = new Swiper('.product__gallery', {
+  loop:true,
+  spaceBetween:0,
+  pagination: {
+      el: '.swiper-pagination'
+  }
+})
+let itemsSlider = new Swiper('.items__slider', {
+  loop:true,
+  spaceBetween:20,
+  slidesPerView: 6,
+  pagination: {
+      el: '.swiper-pagination'
+  },
+  navigation: {
+      nextEl: '.items__arrows .swiper-button-next',
+      prevEl: '.items__arrows .swiper-button-prev',
+  },
+})
+let clientsSlider = new Swiper('.clients__slider', {
+  loop:true,
+  spaceBetween:20,
+  slidesPerView: 6,
+  pagination: {
+      el: '.swiper-pagination'
+  },
+  navigation: {
+      nextEl: '.clients__arrows .swiper-button-next',
+      prevEl: '.clients__arrows .swiper-button-prev',
+  },
+})
