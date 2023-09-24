fd63532285cbb44ceff9d166f875ab4c458ecf03
Kistanov Daniil <rizzydizzy@mail.ru>
2023-09-24 22:59:54 +0300

    Детальная страница

--- src/js/script.js
+++ src/js/script.js
@@ -25,16 +25,28 @@ let productGallery = new Swiper('.product__gallery', {
       el: '.swiper-pagination'
   }
 })
-let itemsSlider = new Swiper('.items__slider', {
+let itemsSlider = new Swiper('.items_first-type .items__slider', {
   loop:true,
   spaceBetween:20,
   slidesPerView: 6,
   pagination: {
-      el: '.swiper-pagination'
+      el: '.items_first-type .swiper-pagination'
   },
   navigation: {
-      nextEl: '.items__arrows .swiper-button-next',
-      prevEl: '.items__arrows .swiper-button-prev',
+      nextEl: '.items_first-type .items__arrows .swiper-button-next',
+      prevEl: '.items_first-type .items__arrows .swiper-button-prev',
+  },
+})
+let itemsSliderSecond = new Swiper('.items_second-type .items__slider', {
+  loop:true,
+  spaceBetween:20,
+  slidesPerView: 5,
+  pagination: {
+      el: '.items_second-type .swiper-pagination'
+  },
+  navigation: {
+      nextEl: '.items_second-type .items__arrows .swiper-button-next',
+      prevEl: '.items_second-type .items__arrows .swiper-button-prev',
   },
 })
 let clientsSlider = new Swiper('.clients__slider', {
@@ -49,3 +61,17 @@ let clientsSlider = new Swiper('.clients__slider', {
       prevEl: '.clients__arrows .swiper-button-prev',
   },
 })
+
+var detailGalleryThumb = new Swiper(".detail__thumbs-slider", {
+  spaceBetween: 10,
+  slidesPerView: 'auto',
+  freeMode: true,
+  watchSlidesProgress: true,
+  direction: "vertical",
+});
+var detailGallery = new Swiper(".detail__pic-slider", {
+spaceBetween: 10,
+thumbs: {
+  swiper: detailGalleryThumb,
+},
+});
