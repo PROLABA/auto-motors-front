8192c8e11a5100022ac510a4c9a7764d6ce8389b
Kistanov Daniil <rizzydizzy@mail.ru>
2023-09-28 15:07:20 +0300

    Попап окна и меню каталога

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,29 @@
+function getScrollBarWidth () {
+  var inner = document.createElement('p');
+  inner.style.width = "100%";
+  inner.style.height = "200px";
+
+  var outer = document.createElement('div');
+  outer.style.position = "absolute";
+  outer.style.top = "0px";
+  outer.style.left = "0px";
+  outer.style.visibility = "hidden";
+  outer.style.width = "200px";
+  outer.style.height = "150px";
+  outer.style.overflow = "hidden";
+  outer.appendChild (inner);
+
+  document.body.appendChild (outer);
+  var w1 = inner.offsetWidth;
+  outer.style.overflow = 'scroll';
+  var w2 = inner.offsetWidth;
+  if (w1 == w2) w2 = outer.clientWidth;
+
+  document.body.removeChild (outer);
+
+  return (w1 - w2);
+};
+
 let bannerSlider = new Swiper('.banners-slider', {
   spaceBetween:32,
   pagination: {
@@ -110,3 +136,21 @@ $('.nav--item-head').on('click',function(){
   $(this).closest('.nav').toggleClass('nav_opened')
 })
 
+$('.header__catalog-btn').on('click',function(){
+  let body = $('body');
+  let scrollBarWidth = getScrollBarWidth();
+  if (body.hasClass('menu_opened')) {
+    body.css('padding-right','0')
+  } else {
+    body.css('padding-right', scrollBarWidth + 'px')
+  }
+  body.toggleClass('menu_opened');
+})
+$('[data-popup]').click(function(){
+  let idPopup = '#' + $(this).attr('data-popup');
+	$.fancybox.open({
+		src: idPopup,
+		type: 'inline',
+    touch: false
+	});
+});
