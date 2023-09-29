7a130734c15ffd52595fcf4c0ee5f0dfbb79154d
Kistanov Daniil <rizzydizzy@mail.ru>
2023-09-29 15:02:12 +0300

    Деталка адаптив

--- src/js/script.js
+++ src/js/script.js
@@ -52,13 +52,13 @@ let productGallery = new Swiper('.product__gallery', {
   }
 })
 
-let itemsSlider,itemsSliderSecond,clientsSlider;
+let itemsSlider,clientsSlider;
 function initDeskSliders() {
   if (document.querySelector(".items_first-type .items__slider")) {
     itemsSlider = new Swiper('.items_first-type .items__slider', {
       loop:true,
       spaceBetween:20,
-      slidesPerView: 6,
+      slidesPerView: 4,
       pagination: {
           el: '.items_first-type .swiper-pagination'
       },
@@ -66,27 +66,19 @@ function initDeskSliders() {
           nextEl: '.items_first-type .items__arrows .swiper-button-next',
           prevEl: '.items_first-type .items__arrows .swiper-button-prev',
       },
+      breakpoints: {
+        1280: {
+          slidesPerView: 6,
+        }
+      }
     })
   }
-  if (document.querySelector(".items_second-type .items__slider")) {
-    itemsSliderSecond = new Swiper('.items_second-type .items__slider', {
-      loop:true,
-      spaceBetween:20,
-      slidesPerView: 5,
-      pagination: {
-          el: '.items_second-type .swiper-pagination'
-      },
-      navigation: {
-          nextEl: '.items_second-type .items__arrows .swiper-button-next',
-          prevEl: '.items_second-type .items__arrows .swiper-button-prev',
-      },
-    })
-  }
+
   if (document.querySelector(".clients__slider")) {
     clientsSlider = new Swiper('.clients__slider', {
       loop:true,
       spaceBetween:20,
-      slidesPerView: 6,
+      slidesPerView:4,
       pagination: {
           el: '.swiper-pagination'
       },
@@ -94,6 +86,11 @@ function initDeskSliders() {
           nextEl: '.clients__arrows .swiper-button-next',
           prevEl: '.clients__arrows .swiper-button-prev',
       },
+      breakpoints: {
+        1280: {
+          slidesPerView: 6,
+        }
+      }
     })
   }
 }
@@ -103,33 +100,55 @@ const resizeHandlerSlider = () => {
         itemsSlider.destroy();
         itemsSlider = null
       }
-      if (itemsSliderSecond) {
-        itemsSliderSecond.destroy();
-        itemsSliderSecond = null
-      }
+
       if (clientsSlider) {
         clientsSlider.destroy();
         clientsSlider = null
       }
   } else {
-    if (!itemsSlider && !itemsSliderSecond && !clientsSlider) {
+    if (!itemsSlider  && !clientsSlider) {
       initDeskSliders();
     }
   }
 }
 window.addEventListener('resize', resizeHandlerSlider)
 resizeHandlerSlider();
+let itemsSliderSecond = new Swiper('.items_second-type .items__slider', {
+  loop:true,
+  spaceBetween:12,
+  slidesPerView: 2,
+  pagination: {
+      el: '.items_second-type .swiper-pagination'
+  },
+  navigation: {
+      nextEl: '.items_second-type .items__arrows .swiper-button-next',
+      prevEl: '.items_second-type .items__arrows .swiper-button-prev',
+  },
+  breakpoints: {
+    970: {
+      slidesPerView: 4,
+      spaceBetween: 20
+    },
+    1280: {
+      slidesPerView: 5,
+    }
+  }
+})
 var detailGalleryThumb = new Swiper(".detail__thumbs-slider", {
   spaceBetween: 10,
   slidesPerView: 'auto',
   freeMode: true,
   watchSlidesProgress: true,
   direction: "vertical",
+
 });
 var detailGallery = new Swiper(".detail__pic-slider", {
 spaceBetween: 10,
 thumbs: {
   swiper: detailGalleryThumb,
+},
+pagination: {
+  el: '.detail__pic-slider .swiper-pagination'
 },
 });
 $('.nav--item-head').on('click',function(){
